09e46f9c846f3078c3de1b6451b4fcb44f772938
ray33ee <30669752+ray33ee@users.noreply.github.com>
2020-04-28 19:45:32 +0100

    Version 2.1.2 - Meaningful return codes and stdout output in JSON form
    Signed-off-by: ray33ee <30669752+ray33ee@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -16,9 +16,30 @@
 #include <unistd.h>
 #endif
 
+//Error codes
+const int INVALID_FILE_HANDLE				= -10;
+const int SET_FILE_POINTER_ERROR			= -11;
+const int SET_END_OF_FILE_ERROR				= -12;
+const int CLOSE_HANDLE_ERROR				= -13;
+const int LINUX_TRUNCATE_ERROR				= -14;
+const int APPEND_FILE_NAME_ERROR			= -15;
+const int OPEN_FILE_ERROR					= -16;
+const int READ_DATA_ERROR					= -17;
+const int WRITE_DATA_ERROR					= -18;
+const int READ_HEADER_ERROR					= -19;
+const int WRITE_HEADER_ERROR				= -20;
+const int READ_BITMAP_ERROR					= -21;
+const int INVALID_BITMAP_BM					= -22;
+const int INVALID_BITMAP_PADDING_VALUE		= -23;
+const int INVALID_BITMAP_BTB				= -24;
+const int INVALID_BITMAP_METADATA			= -25;
+const int NO_INPUT_FILE_ARGUMENT			= -26;
+const int TOO_MANY_ARGUMENTS				= -27;
+const int TRUNCATE_FILE_NAME_ERROR			= -28;	
+const int WIP_INVALID_BITMAP_HEADER 		= -29;
 
 
-#define BYTES_PER_PIXEL (4) // The number of bytes used to represent the length of padding
+#define BYTES_PER_PIXEL (4) // The number of bytes per pixel
 #define BMP_HEADER_SIZE (14) //Size of the actual BMP header (NOT the DIB header) in bytes
 #define MAX_STRING_SIZE (150) //Since all string handling is performed on static array allocated strings, a variable containing the max string size is declared
 
@@ -88,6 +109,53 @@ struct CompleteHeader
 	struct BTBHeader btb;
 } completeHeader;
 
+struct JsonData
+{
+	char* source;
+	char* destination;
+	char* direction;
+	int width;
+	int height;
+	int bpp;
+	int filesize;
+	int padding;
+} jsonData;
+
+// In order to comply with the JSON format, we escape our back slash characters
+void printEscapedPath(char* path)
+{
+	while (*path != '\0')
+	{
+		if (*path == '\\')
+			printf("\\\\");
+		else
+			printf("%c", *path);
+		
+		path++;
+	}
+}
+
+// Output jsonData struct as json string to stdout
+void printJSON()
+{
+	
+	printf("{\n    \"source\": \"");
+	printEscapedPath(jsonData.source);
+	printf("\",\n");
+	
+	printf("    \"destination\": \"");
+	printEscapedPath(jsonData.destination);
+	printf("\",\n");
+	
+	printf("    \"destination\": \"%s\",\n", jsonData.destination);
+	printf("    \"output\": \"%s\",\n", jsonData.direction);
+	printf("    \"pixmap\": {\n        \"width\": %i,\n        \"height\":  %i,\n        \"bpp\": %i\n    },\n", jsonData.width, jsonData.height, (BYTES_PER_PIXEL * 8));
+	printf("    \"filesize\": %i,\n", jsonData.filesize);
+	printf("    \"headersize\": %i,\n", sizeof(struct BitmapV5Header));
+	printf("    \"padding\": %i\n", jsonData.padding);
+	printf("}");
+}
+
 //Platform independent function to resize file at path to size bytes
 int resizefile(const char* path, int size)
 {
@@ -98,35 +166,35 @@ int resizefile(const char* path, int size)
 	if (file == INVALID_HANDLE_VALUE)
 	{
 		fprintf(stderr, "Error: Windows API returned error code %li. Visit 'https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes' for the error code description.\n", GetLastError());
-		return 1;
+		return INVALID_FILE_HANDLE;
 	}
 	
 	//Set file pointer to new file size
 	if (SetFilePointer(file, size, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
 	{
 		fprintf(stderr, "Error: Windows API returned error code %li. Visit 'https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes' for the error code description.\n", GetLastError());
-		return 1;
+		return SET_FILE_POINTER_ERROR;
 	}
 	
 	//Set end of file to position of file pointer
 	if (!SetEndOfFile(file))
 	{
 		fprintf(stderr, "Error: Windows API returned error code %li. Visit 'https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes' for the error code description.\n", GetLastError());
-		return 1;
+		return SET_END_OF_FILE_ERROR;
 	}
 	
 	//Close file
 	if (!CloseHandle(file))
 	{
 		fprintf(stderr, "Error: Windows API returned error code %li. Visit 'https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes' for the error code description.\n", GetLastError());
-		return 1;
+		return CLOSE_HANDLE_ERROR;
 	}
 
 #else
 	if (truncate(path, size))
 	{
 		fprintf(stderr, "Error: Linux call to truncate() failed with error code %i - %s\n", errno, strerror(errno));
-		return 1;
+		return LINUX_TRUNCATE_ERROR;
 	}
 	
 #endif
@@ -143,7 +211,7 @@ int appendfilename(const char* path)
 	if (rename(path, buffer))
 	{
 		fprintf(stderr, "Error: Could not append file name. Error %i - %s\n", errno, strerror(errno));
-		return 1;
+		return APPEND_FILE_NAME_ERROR;
 	}
 	
 	return 0;
@@ -159,7 +227,7 @@ int truncatefilename(const char* path)
 	if (rename(path, buffer))
 	{
 		fprintf(stderr, "Error: Could not truncate file name. Error %i - %s\n", errno, strerror(errno));
-		return 1;
+		return TRUNCATE_FILE_NAME_ERROR;
 	}
 	
 	return 0;
@@ -189,11 +257,11 @@ int fsize(FILE* file)
 
 //Convert source to bitmap file in destination
 int convertToBmp(const char* source)
-{
-	printf("Converting from binary to bmp...\n");
-	
+{	
 	//Open input file to read
 	FILE* binary;
+	
+	int result;
 	
 	binary = fopen(source, "rb+");
 	
@@ -203,39 +271,31 @@ int convertToBmp(const char* source)
 			fprintf(stderr, "Error in convertToBmp %i - %s \"%s\"\n", errno, strerror(errno), source);
 		else
 			fprintf(stderr, "Error in convertToBmp %i - %s\n", errno, strerror(errno));
-		return -2;
+		return OPEN_FILE_ERROR;
 	}
 	
 	//Get size of file to convert
-	int filesize = fsize(binary);
+	jsonData.filesize = fsize(binary);
 	
 	//Calculate the width and height of the pixmap 
-	int side_width = getWidth(filesize);
+	jsonData.width = getWidth(jsonData.filesize);
 	
-	int side_height = getHeight(filesize, side_width);
-	
-#ifdef _DEBUG
-	printf("Pixmap size: %i x %i\n", side_width, side_height);
-#endif
+	jsonData.height = getHeight(jsonData.filesize, jsonData.width);
 	
 	//Calculate padding required for bitmap
-	long long pixmap_size = (long long)side_width * (long long)side_height * BYTES_PER_PIXEL;
+	long long pixmap_size = (long long)jsonData.width * (long long)jsonData.height * BYTES_PER_PIXEL;
 	
-	btbHeader.paddingSize = pixmap_size - filesize - sizeof(btbHeader);
+	btbHeader.paddingSize = pixmap_size - jsonData.filesize - sizeof(btbHeader);
 	
-#ifdef _DEBUG
-	printf("Padding size: %i\n", btbHeader.paddingSize);
-	printf("File size: %i\n", filesize);
-#endif
+	jsonData.padding = btbHeader.paddingSize;
 	
 	//Extend file if smaller than sizeof(completeHeader)
-	if (filesize < sizeof(completeHeader))		
+	if (jsonData.filesize < sizeof(completeHeader))		
 	{
 		fclose(binary);
-		if (resizefile(source, sizeof(completeHeader)))
-		{
-			return 1;
-		}
+		result = resizefile(source, sizeof(completeHeader));
+		if (result)
+			return result;
 		fopen(source, "rb+");
 	}
 	
@@ -244,7 +304,7 @@ int convertToBmp(const char* source)
 	if (ferror(binary))
 	{
 		fprintf(stderr, "Error: Could not read first block for\n");
-		return 1;
+		return READ_DATA_ERROR;
 	}
 	
 	//Append the copied bytes to file
@@ -254,17 +314,14 @@ int convertToBmp(const char* source)
 	if (ferror(binary))
 	{
 		fprintf(stderr, "Error: Could not append first block\n");
-		return 1;
+		return READ_DATA_ERROR;
 	}
 	
-#ifdef _DEBUG
-	printf("Head file: %i\n", (int)sizeof(completeHeader));
-#endif
 	
 	//Modify BMP template header
 	Bitmap_Header.fileSize = pixmap_size + sizeof(Bitmap_Header);
-	Bitmap_Header.width = side_width;
-	Bitmap_Header.height = side_height;
+	Bitmap_Header.width = jsonData.width;
+	Bitmap_Header.height = jsonData.height;
 	Bitmap_Header.pixmapSize = pixmap_size;
 	
 	//Go to beginning of stream
@@ -279,29 +336,31 @@ int convertToBmp(const char* source)
 	if (ferror(binary))
 	{
 		fprintf(stderr, "Error: Could not write BMP or BTB headers\n");
-		return 1;
+		return WRITE_DATA_ERROR;
 	}
 		
 	//Close write file
 	fclose(binary);
 	
 	//Resize to add padding
-	if (resizefile(source, pixmap_size + sizeof(Bitmap_Header)))
-		return 1;
+	result = resizefile(source, pixmap_size + sizeof(Bitmap_Header));
+	if (result)
+		return result;
 	
 	//Add .bmp to filename
-	if (appendfilename(source))
-		return 1;
+	result = appendfilename(source);
+	if (result)
+		return result;
 	
 	return 0;
 }
 
 //Convert source bitmap back to binary file in destination
 int convertToBinary(const char* source)
-{
-	printf("Convert bmp to binary file\n");
-	
+{	
 	FILE* bitmap;
+	
+	int result;
 	
 	//Open bitmap file
 	bitmap = fopen(source, "rb+");
@@ -312,49 +371,43 @@ int convertToBinary(const char* source)
 			fprintf(stderr, "Error in convertToBinary %i - %s \"%s\"\n", errno, strerror(errno), source);
 		else
 			fprintf(stderr, "Error in convertToBinary %i - %s\n", errno, strerror(errno));
-		return -1;
+		return OPEN_FILE_ERROR;
 	}	
 	
 	//Load bitmap header into memory
-	int filesize = fsize(bitmap);
+	jsonData.filesize = fsize(bitmap);
 	
-	int headerBytesRead = fread(&completeHeader, 1, sizeof(completeHeader), bitmap);
-	
-	fprintf(stderr, "Complete header read: %i bytes\n", headerBytesRead);
+	fread(&completeHeader, 1, sizeof(completeHeader), bitmap);
 	
 	if (ferror(bitmap))
 	{
 		fprintf(stderr, "Error: Could not read bitmap header\n");
-		return 1;
+		return READ_HEADER_ERROR;
 	}
 	
 	//Get the size of padding
-	int padding_size = completeHeader.btb.paddingSize;
-	
-#ifdef _DEBUG
-	printf("Padding: %i\n", padding_size);
-#endif
+	jsonData.padding = completeHeader.btb.paddingSize;
 		
 	//Perform some tests to make sure file is valid BTB bitmap
 	//		Check first few characters are BM
 	if (completeHeader.bmp.ID != Bitmap_Header.ID)
 	{
 		fprintf(stderr, "Error: Invalid bitmap file (No BM signature)\n");
-		return 1;
+		return INVALID_BITMAP_BM;
 	}
 	
 	//		Check padding isnt larger than pixmap
-	if (padding_size >= completeHeader.bmp.pixmapSize)
+	if (jsonData.padding >= completeHeader.bmp.pixmapSize)
 	{
 		fprintf(stderr, "Error: Invalid bitmap file (bad padding value)\n");
-		return 1;
+		return INVALID_BITMAP_PADDING_VALUE;
 	}
 
 	//		Check signature
 	if (memcmp(&completeHeader.btb.signature, &btbHeader.signature, sizeof(btbHeader.signature)))
 	{
 		fprintf(stderr, "Error: Invalid bitmap file (bad BTB signature)\n");
-		return 1;
+		return INVALID_BITMAP_BTB;
 	}
 	
 	//		Compare the original header size (the size of the header of the bitmap created directly by tobmp). If different, another program has converted the bitmap to a different kind.
@@ -371,35 +424,29 @@ int convertToBinary(const char* source)
 			//New header is larger than original, so file must be extended
 		}
 		
-		return 1;
+		return WIP_INVALID_BITMAP_HEADER;
 	}
+	
+	jsonData.width = completeHeader.bmp.width;
+	jsonData.height = completeHeader.bmp.height;
 	
 	//		Check to make sure width, height, bpp, pixmap size, bmp and dib header size and actual size all agree
 	
 	int calculatedSize = completeHeader.bmp.width * completeHeader.bmp.height * (completeHeader.bmp.bpp / 8) + completeHeader.bmp.DIBSize + BMP_HEADER_SIZE;	
 	
-	if (calculatedSize != filesize || completeHeader.bmp.fileSize != filesize)
+	if (calculatedSize != jsonData.filesize || completeHeader.bmp.fileSize != jsonData.filesize)
 	{
 		fprintf(stderr, "Error: Invalid bitmap file (bad bitmap metadata)\n");
-		return 1;
+		return INVALID_BITMAP_METADATA;
 	}
 	
 	
 	//Get the location of the head and  the size of the original file
-	int headLocation = filesize - padding_size - btbHeader.originalHeaderSize - sizeof(struct BTBHeader);
-	int originalSize = filesize - padding_size - completeHeader.bmp.offset - sizeof(struct BTBHeader);
-	
-	fprintf(stderr, "filesize: %i\n", filesize);
-	fprintf(stderr, "padding_size: %i\n", padding_size);
-	fprintf(stderr, "originalHeaderSize: %i\n", btbHeader.originalHeaderSize);
-	fprintf(stderr, "BTB headaer: %i\n", sizeof(struct BTBHeader));
+	int headLocation = jsonData.filesize - jsonData.padding - btbHeader.originalHeaderSize - sizeof(struct BTBHeader);
+	int originalSize = jsonData.filesize - jsonData.padding - completeHeader.bmp.offset - sizeof(struct BTBHeader);
 	
 	if (originalSize < btbHeader.originalHeaderSize + sizeof(struct BTBHeader)) //If the original size is smaller than the header, then the headLocation must be at the end of the completeHeader
 	headLocation = completeHeader.bmp.offset + sizeof(struct BTBHeader);
-	
-#ifdef _DEBUG
-	printf("Head Location: %i\n", headLocation);
-#endif
 	
 	//Go to the head
 	fseek(bitmap, headLocation, SEEK_SET);
@@ -411,12 +458,8 @@ int convertToBinary(const char* source)
 	if (ferror(bitmap))
 	{
 		fprintf(stderr, "Error: Could not read head\n");
-		return 1;
+		return READ_HEADER_ERROR;
 	}
-	
-#ifdef _DEBUG
-	printf("Bytes read: %i\n", bytesRead);
-#endif
 	
 	//Go to beginning
 	fseek(bitmap, 0, SEEK_SET);
@@ -427,70 +470,69 @@ int convertToBinary(const char* source)
 	if (ferror(bitmap))
 	{
 		fprintf(stderr, "Error: Could not write head to beginning\n");
-		return 1;
+		return WRITE_HEADER_ERROR;
 	}
 	
 	//Close file
 	fclose(bitmap);
 	
 	//Truncate file to remove padding
-	if (resizefile(source, originalSize))
-		return 1;
+	result = resizefile(source, originalSize);
+	if (result)
+		return result;
 	
 	//Rename file
-	if (truncatefilename(source))
-		return 1;
+	result = truncatefilename(source);
+	if (result)
+		return result;
 	
 	return 0;
 }
 
 int main(int argc, char **argv)
-{	
-	fprintf(stderr, "sizeof %i\n", sizeof(Bitmap_Header));
+{		
 	
-	char* source;
-	
+	jsonData.destination = "";
+		
 	if (argc == 1) //No command line arguments supplied, error.
 	{
 		fprintf(stderr, "Error: No input file. Program terminated.\n");
-		return 1;
+		return NO_INPUT_FILE_ARGUMENT;
 	}
 	else if (argc == 2) //One argument supplied.
 	{
-		source = argv[1];
+		jsonData.source = argv[1];
 	}
 	else if (argc >= 3) // two args. Invalid
 	{		
 		fprintf(stderr, "Error: Only a single argument is accepted.\n");
-	}	
-	
-#ifdef _DEBUG
-	printf("Source     : %s\n", source);
-#endif
+		return TOO_MANY_ARGUMENTS;
+	}
+
 	
 	//Get last 4 characters of source as string
-	char* sub = &source[strlen(source) - 4];
+	char* sub = &jsonData.source[strlen(jsonData.source) - 4];
 	
 	int result;
 	
 	if (strcmp(sub, ".bmp") == 0)
 	{
-		printf("Direction  : Bmp to Bin\n");
-		result = convertToBinary(source);
+		jsonData.direction = "binary";
+		result = convertToBinary(jsonData.source);
 	}
 	else
 	{
-		printf("Direction  : Bin to Bmp\n");
-		result = convertToBmp(source);
+		jsonData.direction = "bitmap";
+		result = convertToBmp(jsonData.source);
 	}
 	
 	if (result)
 	{
 		fprintf(stderr, "Conversion failed.\n");
-		return 1;
+		return result;
 	}
-		
-	printf("Conversion complete\n");
+	
+	printJSON();
 	
 	
 	return 0;
